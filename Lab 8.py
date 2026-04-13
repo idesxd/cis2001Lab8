3d8012029f3beddc6eb9b0eea50b11af3c6b3cba
idesxd <idesai@umich.edu>
2026-04-13 12:58:58 -0400

    final

--- Lab 8.py
+++ Lab 8.py
@@ -1,38 +1,41 @@
-def merge_sort(arr):
+def merge_sort(numbers):
     """Sort a list using merge sort and return a new sorted list."""
-    if len(arr) <= 1:
-        return arr
 
-    middle = len(arr) // 2
-    left_half = merge_sort(arr[:middle])
-    right_half = merge_sort(arr[middle:])
+    if len(numbers) <= 1:
+        return numbers
 
-    return merge(left_half, right_half)
+    midpoint = len(numbers) // 2
+    left_half = merge_sort(numbers[:midpoint])
+    right_half = merge_sort(numbers[midpoint:])
+
+    return merge_lists(left_half, right_half)
 
 
-def merge(left, right):
+def merge_lists(left_list, right_list):
     """Merge two sorted lists into one sorted list."""
-    merged = []
-    i = 0
-    j = 0
 
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged.append(left[i])
-            i += 1
+    merged_list = []
+    left_position = 0
+    right_position = 0
+
+    while left_position < len(left_list) and right_position < len(right_list):
+
+        if left_list[left_position] <= right_list[right_position]:
+            merged_list.append(left_list[left_position])
+            left_position += 1
         else:
-            merged.append(right[j])
-            j += 1
+            merged_list.append(right_list[right_position])
+            right_position += 1
 
-    merged.extend(left[i:])
-    merged.extend(right[j:])
+    merged_list.extend(left_list[left_position:])
+    merged_list.extend(right_list[right_position:])
 
-    return merged
+    return merged_list
 
 
 user_input = input("Enter numbers separated by spaces: ")
 
-numbers = [int(x) for x in user_input.split()]
+numbers = [int(value) for value in user_input.split()]
 
 sorted_numbers = merge_sort(numbers)
 
