b154dea5a16b4c2be60490ecaeae8e13ab96ad08
idesxd <idesai@umich.edu>
2026-04-13 12:56:21 -0400

    update

---
+++ Lab 8.py
@@ -0,0 +1,30 @@
+def merge_sort(arr):
+    """Sort a list using merge sort and return a new sorted list."""
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left_half = merge_sort(arr[:middle])
+    right_half = merge_sort(arr[middle:])
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    """Merge two already-sorted lists into one sorted list."""
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+
+    return merged
